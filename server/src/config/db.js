a9c49095367617a2e8d28ec554b323a878f5e0f2
Rishi Yadav <113158883+rishiyadavdesign@users.noreply.github.com>
2026-06-10 03:20:04 +0530

    Validate MONGO_URI scheme and clearer error message

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,8 +1,19 @@
 import mongoose from 'mongoose';
 
+function isValidMongoUri(uri) {
+  return typeof uri === 'string' && (uri.startsWith('mongodb://') || uri.startsWith('mongodb+srv://'));
+}
+
 export async function connectDB() {
   const uri = process.env.MONGO_URI;
-  if (!uri) throw new Error('MONGO_URI is required');
+  if (!uri) {
+    throw new Error('MONGO_URI is required. Set MONGO_URI as an environment variable. Example: mongodb+srv://user:password@cluster0.mongodb.net/mydb');
+  }
+
+  if (!isValidMongoUri(uri)) {
+    throw new Error('Invalid MONGO_URI provided. Expected scheme to start with "mongodb://" or "mongodb+srv://"');
+  }
+
   await mongoose.connect(uri);
   console.log('MongoDB connected');
 }
